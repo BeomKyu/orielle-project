a3d1e644221b9982eef20471831fc2da0a3b8069
beomkyu <gw030914@gmail.com>
2025-08-17 17:57:27 +0900

    # first commit

---
+++ backend/orielle-idessy-service/build.gradle.kts
@@ -0,0 +1,17 @@
+//plugins {
+//    kotlin("jvm")
+//}
+
+group = "com.orielle"
+version = "unspecified"
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "orielle-project"
+
+// 서브 모듈 빌드 스크립트
+include("backend:orielle-idessy-service")
+include("backend:orielle-friend-service")
