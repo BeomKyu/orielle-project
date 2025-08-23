a2f4f4d0fd5174755161f90120421ea9cccc5e45
beomkyu <gw030914@gmail.com>
2025-08-23 23:53:25 +0900

    feat: build.gradle.kts
    - add base dependency for orielle project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,11 @@
 
 // 모든 하위 프로젝트(모듈)에 적용될 플러그인과 설정을 정의합니다.
 subprojects {
+    // 공통 플러그인 적용
+    apply(plugin = "buildsrc.convention.kotlin-jvm")
+
+    // 공통 저장소 설정
+    repositories {
+        mavenCentral()
+    }
 }
